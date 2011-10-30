f23a00178c0f7aa6ddac75d44599b279475a3d39
chony <chony@ubuntu.(none)>
2011-10-30 13:28:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Century21ap::Application.config.secret_token = '5fcb8e1a6b322f0bc1157c937e989be04d9dba4beb5e888f7b80426ba9f4f0a8396f0ceb621081e5be6dc5f121d9825f2783aa512ed79fb39e3c9c8cd901faf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Century21ap::Application.config.session_store :cookie_store, key: '_century21ap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Century21ap::Application.config.session_store :active_record_store
