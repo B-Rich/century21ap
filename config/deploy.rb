51b65eb8cb2b13597cd237538fc746a39229973a
Jack Desert <jack@healthtalker.com>
2011-11-07 06:48:44 -0500

    updated deploy.rb with miconecta settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,32 @@
-# The name of your app
-set :application, "century21ap"
-# The directory on the EC2 node that will be deployed to
-set :deploy_to, "rails/#{application}"
-# The type of Source Code Management system you are using
+set :application, "miconecta"
+set :repository,  "wim@wim.webfactional.com:repos/miconecta.git"
+set :deploy_to, "/home/wim/webapps/miconnecta"
 set :scm, :git
-# The location of the LOCAL repository relative to the current app
-set :repository,  ".git"
-# The way in which files will be transferred from repository to remote host
-# If you were using a hosted github repository this would be slightly different
-set :deploy_via, :copy
 
-# The address of the remote host on EC2 (the Public DNS address)
-set :location, "ubuntu@ec2-50-17-70-234.compute-1.amazonaws.com"
-# setup some Capistrano roles
-role :app, location
-role :web, location
-role :db,  location, :primary => true
+role :web, "web219.webfaction.com"                          # Your HTTP server, Apache/etc
+role :app, "web219.webfaction.com"                          # This may be the same as your `Web` server
+role :db,  "web219.webfaction.com", :primary => true # This is where Rails migrations will run
 
-# Set up SSH so it can connect to the EC2 node - assumes your SSH key is in ~/.ssh/id_rsa
-set :user, "ubuntu"
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]	
+set :user, "wim"
+set :scm_username, "wim"
+set :use_sudo, false
+default_run_options[:pty] = true
+
+set :rvm_type, :user
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+
+# require "bundler/capistrano"
+# require "rvm/capistrano"
+
+task :qa do
+  set :branch, %x(git branch | grep '^*' | colrm 1 2).strip
+  role :app, "web220.webfaction.com"
+end
+
+namespace :deploy do
+  desc "Restart nginx"
+  task :restart do
+    run "#{deploy_to}/bin/restart"
+  end
+end
+
